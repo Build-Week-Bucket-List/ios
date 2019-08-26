a01a484e05de4c88dd585fca2bdf1bbc76a3d6ce
Jordan Christensen <jordan.c4922@gmail.com>
2019-08-27 03:47:05 +0800

    Implement BucketList TableVC UI

---
+++ Bucket List/Storyboards/Base.lproj/BucketListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  BucketListViewController.swift
+//  Bucket List
+//
+//  Created by Jordan Christensen on 8/27/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class BucketListViewController: UIViewController, UITableViewDataSource {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BucketListCell", for: indexPath) as? BucketListTableViewCell else { return UITableViewCell() }
+        
+        cell.bucketListItemName.text = "Name"
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
