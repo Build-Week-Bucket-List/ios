54afcdcb032a2edd4cf8161394b9560825e3ddec
Dani Blackwell <bleuxgenius@gmail.com>
2019-08-28 15:23:41 -0500

    UI layout for SearchBar

---
+++ Bucket List/View Controllers/FriendsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsTableViewCell.swift
+//  Bucket List
+//
+//  Created by Lambda_School_Loaner_167 on 8/28/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
