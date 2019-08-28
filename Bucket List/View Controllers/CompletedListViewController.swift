7c395021dd8165cafd03d4679d39aca8a08afb9d
Jordan Christensen <jordan.c4922@gmail.com>
2019-08-29 04:05:08 +0800

    Fix crash

--- Bucket List/View Controllers/CompletedListViewController.swift
+++ Bucket List/View Controllers/CompletedListViewController.swift
@@ -16,7 +16,7 @@ class CompletedListViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
         
         completedTableView.dataSource = self
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -33,15 +33,15 @@ class CompletedListViewController: UIViewController, UITableViewDataSource {
         return cell
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
